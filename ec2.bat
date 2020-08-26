eb4a4e90b9659239862d4e706345af02db78fd67
kamal-bk <69685552+kamal-bk@users.noreply.github.com>
2020-08-26 08:18:34 -0700

    Update ec2.bat

--- ec2.bat
+++ ec2.bat
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-07c8bc5c1ce9598c3 --count 1 --instance-type t2.micro --key-name demo --security-groups demo
+aws ec2 run-instances --image-id ami-07c8bc5c1ce9598c3 --count 1 --instance-type t2.micro --key-name demo --security-groups demo --region us-east-2
